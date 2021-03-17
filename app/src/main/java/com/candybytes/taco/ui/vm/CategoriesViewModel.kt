32ca6b27f959e31b1754f90d8ff373fee47752e3
issyzac <issyzac.iz@gmail.com>
2021-03-17 23:04:24 +0300

    Return livedata for all the categories

--- app/src/main/java/com/candybytes/taco/ui/vm/CategoriesViewModel.kt
+++ app/src/main/java/com/candybytes/taco/ui/vm/CategoriesViewModel.kt
@@ -2,11 +2,10 @@ package com.candybytes.taco.ui.vm
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.map
+import androidx.lifecycle.*
 import com.candybytes.taco.api.TacoService
+import com.candybytes.taco.db.FoodDao
+import com.candybytes.taco.vo.Category
 import timber.log.Timber
 
 class CategoriesViewModel @ViewModelInject constructor(
@@ -22,5 +21,16 @@ class CategoriesViewModel @ViewModelInject constructor(
         }
     }.map { "Loaded ${it.size} categories\nImplement a list view and show all category elements." }
 
+
+    private val _allCategories = liveData {
+        try {
+            emit(tacoService.getAllCategoriesAsync())
+        }catch (e: Exception){
+            Timber.e(e)
+        }
+    }
+
+    val allCategoriesData: LiveData<List<Category>>
+        get() = _allCategories
 
 }
