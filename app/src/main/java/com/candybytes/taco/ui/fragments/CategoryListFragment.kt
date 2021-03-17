ea311deb1c3287abbbf999e4459f7f1797fe5d20
issyzac <issyzac.iz@gmail.com>
2021-03-17 23:05:47 +0300

    Display list of category items

--- app/src/main/java/com/candybytes/taco/ui/fragments/CategoryListFragment.kt
+++ app/src/main/java/com/candybytes/taco/ui/fragments/CategoryListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.candybytes.taco.databinding.FragmentCategoryListBinding
+import com.candybytes.taco.ui.adapters.CategoriesAdapter
 import com.candybytes.taco.ui.vm.CategoriesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,10 +22,26 @@ class CategoryListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+
+
         return FragmentCategoryListBinding.inflate(layoutInflater, container, false).apply {
             viewModel = this@CategoryListFragment.viewModel
             lifecycleOwner = this@CategoryListFragment
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoryAdapter = CategoriesAdapter()
+
+        val binding = DataBindingUtil.findBinding<FragmentCategoryListBinding>(view)
+        binding?.adapter = categoryAdapter
+
+        viewModel.allCategoriesData.observe(viewLifecycleOwner, Observer {
+            it.let(categoryAdapter::submitList)
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
